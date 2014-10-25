06701fa144e3f6357f01641e20db7ecf997972e3
Kyle Olive <kolive@uwaterloo.ca>
2014-10-25 17:38:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Personal Nodeserver
+
+This is a simple node application that deals with routing of various subdomains and domains to the correct websites / apps. 
+
+
---
+++ server.js
@@ -0,0 +1,18 @@
+var port = 8080;
+var express = require("express");
+var https = require('https');
+var http = require('http');
+
+//main webserver app
+var main = express();
+
+//load middlware apps
+var site = require("/home/kolive/kyleolive.ca/website.js").app;
+var staging = require("/home/kolive/staging.kyleolive.ca/website.js").app;
+
+main.use(site);
+//main.use(express.vhost("staging.kyleolive.ca", staging));
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+
